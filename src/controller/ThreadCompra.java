eb337f50de1897e0b22a043d2ea59c6d7567327a
usuario <usuario@usuario-PC>
2021-03-19 14:54:58 -0300

    quarto commit

--- src/controller/ThreadCompra.java
+++ src/controller/ThreadCompra.java
@@ -1,5 +1,82 @@
 package controller;
 
-public class ThreadCompra {
+import java.util.concurrent.Semaphore;
 
+public class ThreadCompra extends Thread { 
+	
+	private int quantIngresso = 100;
+	int idCompra;
+	private Semaphore semaforo;
+
+	public ThreadCompra (int idCompra, Semaphore semaforo) {
+		this.idCompra = idCompra;
+		this.semaforo = semaforo;
+	}
+
+	
+
+		@Override
+			public void run() {
+			try {
+				semaforo.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			Comprar();
+			confirmaCompra();
+			
+			
+}
+
+	private void Login () {
+		
+		int tempo = (int)((Math.random()*2000) + 5000);
+		if (tempo >= 1000) 
+		{
+			System.out.println("Infelizmente seu tempo esgotou");	
+		}else {
+			try {
+				Thread.sleep(tempo);
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+		
+			
+		}	
+	}	
+	
+	public void Comprar () {
+	
+		int tempoComprar = (int)((Math.random()*2001)+1000);
+		 if (tempoComprar > 15000) {
+			 System.out.println("tempo estourado, fim de sessão");
+		 }else {
+			 
+			 try {
+				Thread.sleep(tempoComprar);
+			} catch (InterruptedException e) {
+	
+				e.printStackTrace();
+			}
+		 }
+			
+	}
+	
+	public void confirmaCompra () {
+		int ingressosavenda = (int)((Math.random()*4)+1);
+		if (ingressosavenda < quantIngresso) {
+			quantIngresso = quantIngresso - ingressosavenda;
+			
+			System.out.println("Comprador #" + idCompra + " quantidade vendida" + ingressosavenda + "restam" + quantIngresso);
+			
+		}else {
+			System.out.println("Todos os ingressos já foram vendidos");
+		}
+		
+		
+		
+	}
+	
+	
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,10 +1,25 @@
 package view;
 
+import java.lang.management.ThreadInfo;
+import java.util.concurrent.Semaphore;
+
+import controller.ThreadCompra;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	int id = 0;
+	int quantIngresso = 100;
+	Semaphore semaforo = new Semaphore (quantIngresso);
+	for(int i = 0; i < 100 ; i++) {
+		
+		Thread threadcompra = new ThreadCompra (id,semaforo);
+		threadcompra.start();
+		id++;
+		
+		
+	}
 	}
 
 }
